e5112bfd901ac168d5ade88048bb3c54d1686bde
p1kul <funikova@me.com>
2024-12-12 17:29:51 +0300

    Add files via upload

--- Flask/flaska.py
+++ Flask/flaska.py
@@ -1,21 +1,7 @@
 from flask import Flask, render_template, request
-import sqlite3
-
+from models import session, User, Randomaizer
+from random import choice
 app = Flask(__name__, template_folder='templates')
-
-connection = sqlite3.connect('Flask.db', check_same_thread=False)
-cursor = connection.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS User    
-(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-login TEXT UNIQUE,
-password TEXT
-)
-""")
-
-connection.commit()
 
 
 @app.route('/')
@@ -25,37 +11,58 @@ def home():
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    sessions = session()
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
         repeat_password = request.form.get('repeat_password')
         if password != repeat_password:
             return render_template('registration.html',  error='Пароли не совпадают')
-        cursor.execute('SELECT * FROM User WHERE login = ?', (login,))
-        if cursor.fetchone():
+        user = sessions.query(User).filter_by(login=login).first()
+        if user:
             return render_template('registration.html', error='Такой user уже существует')
-        cursor.execute('INSERT INTO User (login, password) VALUES (?, ?)', (login, password))
-        connection.commit()
+        sessions.add(User(login=login, password=password))
+        sessions.commit()
         return render_template('home.html')
     return render_template('registration.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    sessions = session()
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        cursor.execute('SELECT * FROM User WHERE login = ? AND password = ?', (login, password))
-        if not cursor.fetchone():
+        user = sessions.query(User).filter_by(login=login, password=password).first()
+        if not user:
             return render_template('login.html',  error='Что-то пошло не так')
 
         return render_template('main.html')
+    sessions.close()
     return render_template('login.html')
 
 
-@app.route('/main')
+@app.route('/main', methods=['GET', 'POST'])
 def main():
+    sessions = session()
+    if request.method == 'POST':
+        something = request.form.get('something')
+        sessions.add(Randomaizer(something=something))
+        sessions.commit()
+        return render_template('main.html')
     return render_template('main.html')
+
+
+@app.route('/main/randoms', methods=['GET'])
+def randoms():
+    sessions = session()
+    if request.method == 'GET':
+        ran = sessions.query(Randomaizer.something).all()
+        answer = choice(ran)
+        slovo = ''.join(answer)
+        return render_template('randoms.html', message=slovo)
+    sessions.close()
+    return render_template('randoms.html')
 
 
 @app.route('/login/helper')
@@ -70,3 +77,5 @@ def wait():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
